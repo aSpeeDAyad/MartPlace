e8455e32de8638b789deb010268bf92f0a8c1aed
Your NameaSpeeDAyad <dmitryiorzhev@gmail.com>
2020-10-10 19:43:13 +0300

    add section weekly and newest

--- app/js/main.js
+++ app/js/main.js
@@ -2,35 +2,26 @@
 $(function(){
 
     
-   
-    
-    
-        
-       
+    $('.weekly__product-item').slick({});  // подключаем slick-slider
 
-       
+    $('.slick-prev').html('<'); // меняем контент по дефолту с кнопкм слайдера
+    $('.slick-next').html('>'); // 
 
     $('.create__categories-list').on('click', function() {
         $('.category__drop-list').fadeToggle("slow");
     });
-    
 
-    
-   
+    $('.create__categories-list').on('click', function() {
+        
+        $('.arrow').toggleClass('active') ;
+    });
 
-   // Скрыть блок с категориями, по клику вне блока
+   // Скрыть блок с категориями, по клику вне блока и стилизовать положение стрелочки в category-list
     
-    let a = $('.category__drop-list').css('display');
     let btnCategories = $(".create__categories-list"); // указываем кнопку
     let categories = $(".category__drop-list");
     let arrow = $(".arrow");
-     
-    
-        
-      
-    
 
-	
   $(document).on('click',function (e) { // событие клика по веб-документу
     if ( ! btnCategories.is(e.target) && btnCategories.has(e.target).length === 0 &&
          // если клик был не по нашему блоку
@@ -38,23 +29,17 @@ $(function(){
          // и не по его дочерним элементам
            )
             { 
-          
                
-            arrow.removeClass('active'),
-        
-           categories.fadeOut();
-            // скрываем его
+           arrow.removeClass('active'), // удаляем класс active
+           
+           categories.fadeOut();// скрываем категории
+            
         }
-        
-     
+
     });
     
     
-    $('.create__categories-list').on('click', function() {
-        
-        $('.arrow').toggleClass('active') ;
-    });
-      
+    
           
    
 
@@ -63,9 +48,10 @@ $(function(){
     
 
 
-
     
-
+    
+    
+    
 
 
 });
