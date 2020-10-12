32ea476651e2e9e730f5168b7841e3fd0ab2ded5
Your NameaSpeeDAyad <dmitryiorzhev@gmail.com>
2020-10-12 15:54:09 +0300

    add mixItUp to section Newest

--- app/js/main.js
+++ app/js/main.js
@@ -2,24 +2,24 @@
 $(function(){
 
     
-    $('.weekly__product-item').slick({});  // подключаем slick-slider
+    $('.weekly_product-item').slick({});  // подключаем slick-slider
 
     $('.slick-prev').html('<'); // меняем контент по дефолту с кнопкм слайдера
     $('.slick-next').html('>'); // 
 
-    $('.create__categories-list').on('click', function() {
-        $('.category__drop-list').fadeToggle("slow");
+    $('.create_categories-list').on('click', function() {
+        $('.category_drop-list').fadeToggle("slow");
     });
 
-    $('.create__categories-list').on('click', function() {
+    $('.create_categories-list').on('click', function() {
         
         $('.arrow').toggleClass('active') ;
     });
 
    // Скрыть блок с категориями, по клику вне блока и стилизовать положение стрелочки в category-list
     
-    let btnCategories = $(".create__categories-list"); // указываем кнопку
-    let categories = $(".category__drop-list");
+    let btnCategories = $(".create_categories-list"); // указываем кнопку
+    let categories = $(".category_drop-list");
     let arrow = $(".arrow");
 
   $(document).on('click',function (e) { // событие клика по веб-документу
@@ -39,6 +39,9 @@ $(function(){
     });
     
     
+
+
+    var mixer = mixitup('.newest_items');
     
           
    
